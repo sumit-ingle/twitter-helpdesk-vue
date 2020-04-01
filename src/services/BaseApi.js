88fbe331255a848de49ef7315baf2d25d9b3de51
sumit-ingle <61662978+sumit-ingle@users.noreply.github.com>
2020-04-01 16:42:28 +0530

    Update BaseApi.js

--- src/services/BaseApi.js
+++ src/services/BaseApi.js
@@ -6,8 +6,7 @@ export default() => {
         baseURL: `https://vue-twitter-dashboard.herokuapp.com/api`,
         withCredentials: true,
         headers: {
-            'Accept': 'application/json, text/plain, */*',
-            Cookie: 'connect.sid=s%3A9p8VTH228tEfOEEzCAksIMWThTlPs03T.HbVbi6NsGeIVmXvlIAKRipfbufH8WWATMwztPs0MMzM'
+            'Accept': '*/*'
         }
     })
 }
